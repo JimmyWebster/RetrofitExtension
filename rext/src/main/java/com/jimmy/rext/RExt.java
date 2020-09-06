f7b8188039c8e29de5c6b763d93cbd76e1e4db12
JimmyWebster <YukioSid3@mail.ru>
2020-09-06 18:26:51 +0300

    HttpClient

--- rext/src/main/java/com/jimmy/rext/RExt.java
+++ rext/src/main/java/com/jimmy/rext/RExt.java
@@ -1,9 +1,19 @@
 package com.jimmy.rext;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
 public class RExt {
 
-    public static int Test(int a, int b){
-        return a+b;
+    public static OkHttpClient.Builder getHttpClient(){
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(loggingInterceptor);
+
+        return httpClient;
     }
+
+
 
 }
