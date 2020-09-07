c97de8daaf9b23a28f5162f5bfc6375aee87e836
JimmyWebster <YukioSid3@mail.ru>
2020-09-08 00:05:48 +0300

    2.6.4 Version

--- rext/src/main/java/com/jimmy/rext/RExt.java
+++ rext/src/main/java/com/jimmy/rext/RExt.java
@@ -78,32 +78,16 @@ public class RExt {
     }
 
 
-    public static void getDataFromDatabaseOP(String BASE_TAG, final Context context, final List<Object> objectList) {
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference(BASE_TAG);
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                objectList.clear();
-                for (DataSnapshot itemSnapshot : snapshot.getChildren()
-                ) {
-                    Object object = itemSnapshot.getValue(Object.class);
-                    objectList.add(object);
-
-                }
-            }
-
-//            TODO: End this method
-//            itemAdapter = new ItemAdapter(objectList,MainActivity.this);
-//            rv.setAdapter(itemAdapter);
-//            itemAdapter.setOnItemClickListener(MainActivity.this);
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(context, "" + error.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
+    public static List<Object> getDataFromDatabaseOP(String BASE_TAG, final Context context, final List<Object> objectList, DataSnapshot dataSnapshot) {
+        objectList.clear();
+        for (DataSnapshot itemSnapshot: dataSnapshot.getChildren()
+        ) {
+            Object item = itemSnapshot.getValue(Object.class);
+            objectList.add(item);
+        }
 
 
+        return objectList;
     }
 
     public static void updateDataDatabase(String BASE_TAG, String id, Object object) {
@@ -127,123 +111,9 @@ public class RExt {
         drItem.removeValue();
     }
 
-    //    OTP
-    private void sendVerificationFK(String number, PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBack, Activity activity) {
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                number,
-                60,
-                TimeUnit.SECONDS,
-                activity,
-                mCallBack
-        );
 
-    }
-
-    private void verifyCodeFK(String code, String verificationId) {
-        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
-//        signInWithCredentials(credential);
-    }
 
     private void phoneAuthVerifyOTP(){
-//      TODO: Finish this method
-
-//       !!!!onCreate {
-//            super.onCreate(savedInstanceState);
-//            setContentView(R.layout.activity_main);
-//            ButterKnife.bind(this);
-//
-//
-//            mAdView = findViewById(R.id.adView);
-//            AdRequest adRequest = new AdRequest.Builder().build();
-//            mAdView.loadAd(adRequest);
-//            mAuth = FirebaseAuth.getInstance();
-//
-//            if (mAuth.getCurrentUser()!=null){
-//                startActivity(new Intent(getApplicationContext(), VideosActivity.class));
-//            }
-//
-//
-//            btnDone.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//
-//
-//                    String number = etPhone.getText().toString();
-//
-//                    if (!number.isEmpty()) {
-//
-//                        sendVerification(number);
-//
-//                    } else
-//                        Toast.makeText(MainActivity.this, "Empty string", Toast.LENGTH_SHORT).show();
-//                }
-//            });
-//
-//            btnCode.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    String code = etCode.getText().toString();
-//                    if (!code.isEmpty()){
-//                        verifyCode(code);
-//                    }
-//                }
-//            });
-//        }
-
-
-//
-//        private void sendVerification(String number) {
-//            PhoneAuthProvider.getInstance().verifyPhoneNumber(
-//                    number,
-//                    60,
-//                    TimeUnit.SECONDS,
-//                    this,
-//                    mCallBack
-//            );}
-//
-
-//        private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-//            @Override
-//            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-//                super.onCodeSent(s, forceResendingToken);
-//                Toast.makeText(MainActivity.this, "Code sent", Toast.LENGTH_SHORT).show();
-//                verificationId = s;
-//            }
-//
-//            @Override
-//            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-//                Toast.makeText(MainActivity.this, "Automatically verified", Toast.LENGTH_SHORT).show();
-//                if (phoneAuthCredential.getSmsCode() != null)
-//                    verifyCode(phoneAuthCredential.getSmsCode());
-//            }
-//
-//            @Override
-//            public void onVerificationFailed(@NonNull FirebaseException e) {
-//                Toast.makeText(MainActivity.this, "" + e.toString(), Toast.LENGTH_SHORT).show();
-//            }
-//        };
-
-//
-//        private void verifyCode(String code) {
-//            PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
-//            signInWithCredentials(credential);
-//        }
-//
-//        private void signInWithCredentials(PhoneAuthCredential credential) {
-//            FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-//            firebaseAuth.signInWithCredential(credential)
-//                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                        @Override
-//                        public void onComplete(@NonNull Task<AuthResult> task) {
-//                            if (task.isSuccessful()) {
-//                                Toast.makeText(MainActivity.this, "Successful login", Toast.LENGTH_SHORT).show();
-//                                startActivity(new Intent(getApplicationContext(), VideosActivity.class));
-//                            }else{
-//                                Toast.makeText(MainActivity.this, "" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-//                            }
-//                        }
-//                    });
-//        }
 
     }
 
