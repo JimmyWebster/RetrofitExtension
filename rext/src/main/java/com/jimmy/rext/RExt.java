73d566f3d0f907e7126c4f5899ed5697db851eba
JimmyWebster <YukioSid3@mail.ru>
2020-09-08 11:12:21 +0300

    2.6.7 Version

--- rext/src/main/java/com/jimmy/rext/RExt.java
+++ rext/src/main/java/com/jimmy/rext/RExt.java
@@ -1,9 +1,15 @@
 package com.jimmy.rext;
 
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Toast;
 
@@ -24,8 +30,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
@@ -33,6 +43,9 @@ import java.util.concurrent.TimeUnit;
 import butterknife.ButterKnife;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class RExt {
 
@@ -85,7 +98,6 @@ public class RExt {
     }
 
 
-
     public <T> List<T> getDataFromDatabaseOP(Class<T> klazz, DataSnapshot dataSnapshot) {
         List<T> list = new ArrayList<>();
         for (DataSnapshot itemSnapshot : dataSnapshot.getChildren()
@@ -118,7 +130,90 @@ public class RExt {
     }
 
 
-    private void phoneAuthVerifyOTP() {
+    public static Bitmap getPreview(String videoPath)
+            throws Throwable {
+        Bitmap bitmap = null;
+        MediaMetadataRetriever mediaMetadataRetriever = null;
+        try {
+            mediaMetadataRetriever = new MediaMetadataRetriever();
+            mediaMetadataRetriever.setDataSource(videoPath, new HashMap<String, String>());
+
+            bitmap = mediaMetadataRetriever.getFrameAtTime(1, MediaMetadataRetriever.OPTION_CLOSEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Throwable(
+                    "Exception in retriveVideoFrameFromVideo(String videoPath)"
+                            + e.getMessage());
+
+        } finally {
+            if (mediaMetadataRetriever != null) {
+                mediaMetadataRetriever.release();
+            }
+        }
+        return bitmap;
+    }
+
+    public static <T> Uri downloadFile(String url, int position, Context context, List<T> list) {
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_MOVIES, "video" + position + ".mp4");
+
+        long reference = downloadManager.enqueue(request);
+        Uri fileUri;
+        fileUri = downloadManager.getUriForDownloadedFile(reference);
+        if (fileUri != null) {
+            saveDataToPreferences(context, list);
+            return uri;
+        } else {
+            saveDataToPreferences(context, list);
+            return downloadManager.getUriForDownloadedFile(reference);
+        }
+
+
+    }
+
+    public static <T> void saveDataToPreferences(Context context, List<T> list) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("shared preferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        editor.putString("videos", json);
+        editor.apply();
+    }
+
+    public static <T> void loadDataToPreferences(Context context, List<T> list) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("shared preferences", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("videos", null);
+        Type type = new TypeToken<ArrayList<T>>() {
+        }.getType();
+        list = gson.fromJson(json, type);
+
+        if (list == null) {
+            list = new ArrayList<>();
+        }
+
+    }
+
+    public static void requestPermissionStorage(Activity activity,int REQUEST_CODE) {
+        activity.requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, REQUEST_CODE);
+
+
+    }
+
+    public static <T> void filterListViewByParameterOP(List<T> list,Class<T> klazz){
+        List<T> videoListNew = new ArrayList(list);
+        List<T> delete = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+//            if (!list.get(i).isDownloaded())
+                delete.add(list.get(i));
+        }
+
+        videoListNew.removeAll(delete);
+
 
     }
 
