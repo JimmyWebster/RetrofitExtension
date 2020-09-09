329735ce1e300db4bf498e3704a0da8c581439b3
JimmyWebster <YukioSid3@mail.ru>
2020-09-10 01:07:42 +0300

    2.7.1-upd

--- rext/src/main/java/com/jimmy/rext/RExt.java
+++ rext/src/main/java/com/jimmy/rext/RExt.java
@@ -15,6 +15,7 @@
 
 package com.jimmy.rext;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.DownloadManager;
@@ -22,11 +23,14 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.location.Location;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -34,23 +38,62 @@ import android.os.Environment;
 import android.os.Handler;
 import android.util.Log;
 import android.view.WindowManager;
+import android.webkit.MimeTypeMap;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
+import com.cs.googlemaproute.DrawRoute;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -72,19 +115,99 @@ public class RExt {
     }
 
 
-    //  Initialize mobs
-    public static void initializeMob(AdView mAdView, Context context) {
-//
-//        MobileAds.initialize(context, new OnInitializationCompleteListener() {
-//            @Override
-//            public void onInitializationComplete(InitializationStatus initializationStatus) {
-//
-//            }
-//        });
-//
-//        AdRequest adRequest = new AdRequest.Builder().build();
-//        mAdView.loadAd(adRequest);
+    public static String fkGetHttpWebSite(String URL) {
+        try {
+            java.net.URL mySite = new URL(URL);
+            URLConnection yc = mySite.openConnection();
+            Scanner in = new Scanner(new InputStreamReader(yc.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+            while (in.hasNext()) {
+                stringBuilder.append(in.next());
+            }
+            in.close();
+
+            return stringBuilder.toString();
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return null;
+        }
     }
+
+
+    // return sum of ints in list
+    // list may not be null
+    private static int fkSum(int[] list) {
+        assert list != null : "Failed precondition, sum: parameter list" +
+                " may not be null.";
+        int total = 0;
+        for (int x : list) {
+            total += x;
+        }
+        return total;
+    }
+
+
+    // pre: url != null
+    // Connect to the URL specified by the String url.
+    // Map characters to index in array.
+    // All non ASCII character dumped into one element of array
+    // If IOException occurs message printed and array of
+    // length 0 returned.
+    public static int[] fkCreateFreqTableURL(String url) {
+        final int NUM_ASCII_CHAR = 128;
+        if (url == null)
+            throw new IllegalArgumentException("Violation of precondition. parameter url must not be null.");
+
+        int[] freqs = new int[NUM_ASCII_CHAR];
+        try {
+            URL inputURL = new URL(url);
+            InputStreamReader in
+                    = new InputStreamReader(inputURL.openStream());
+
+            while (in.ready()) {
+                int c = in.read();
+                if (0 <= c && c < freqs.length)
+                    freqs[c]++;
+                else
+                    System.out.println("Non ASCII char: " + c + " " + (char) c);
+            }
+            in.close();
+        } catch (MalformedURLException e) {
+            System.out.println("Bad URL.");
+            freqs = new int[0];
+        } catch (IOException e) {
+            System.out.println("Unable to read from resource." + e);
+            freqs = new int[0];
+        }
+        return freqs;
+    }
+
+
+    // Connect to the file specified by the String fileName.
+    // Assumes it is in same directory as compiled code.
+    // Map characters to index in array.
+    public static int[] fkCreateTable(String fileName) throws FileNotFoundException, IOException {
+        final int NUM_ASCII_CHAR = 128;
+        int[] freqs = new int[NUM_ASCII_CHAR];
+        File f = new File(fileName);
+        FileReader r = new FileReader(f);
+        while (r.ready()) {
+            int ch = r.read();
+//            if( 0 <= ch && ch <= NUM_ASCII_CHAR)
+//                freqs[ch]++;
+//            else
+//                freqs[INDEX_NON_ASCII]++;
+            if (0 <= ch && ch < freqs.length)
+                freqs[ch]++;
+            else
+                System.out.println((char) ch);
+
+        }
+        r.close();
+        return freqs;
+    }
+
 
     public static DatabaseReference getReference(String BASE_TAG) {
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference(BASE_TAG);
@@ -376,12 +499,12 @@ public class RExt {
     public static void nfCreateFBMessagingService3(Object remoteMessage, Context context) {
 //        ov: on message received
 //        if (remoteMessage.getData() != null) {
-        sendFictionNew3OP(remoteMessage,context, Activity.class);
+        sendFictionNew3OP(remoteMessage, context, Activity.class);
 
 
     }
 
-    public static <T>void sendFictionNew3OP(Object remoteMessage, Context context, Class<T> clasz) {
+    public static <T> void sendFictionNew3OP(Object remoteMessage, Context context, Class<T> clasz) {
 //        Map<String, String> data = remoteMessage.getData();
         Map<String, String> data = (Map<String, String>) remoteMessage;
         String title = data.get("title");
@@ -399,7 +522,7 @@ public class RExt {
         notificationChannel.enableVibration(false);
         notificationManager.createNotificationChannel(notificationChannel);
 
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context,NOTIFICATION_CHANNEL_ID);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID);
 
 //        new intent
         Intent notificationIntent = new Intent(context, clasz);
@@ -418,11 +541,11 @@ public class RExt {
                 .setContentIntent(contentIntent)
                 .setContentInfo("info");
 
-        notificationManager.notify(1,notificationBuilder.build());
+        notificationManager.notify(1, notificationBuilder.build());
 
     }
 
-    public static void createFbInstanceIdService5(){
+    public static void createFbInstanceIdService5() {
 //        Todo: finish this method with refresh actions
 //        ov: tkn refresh
     }
@@ -432,17 +555,171 @@ public class RExt {
         context.startService(new Intent());
     }
 
-    private void stopServiceOnMainThread(Context context) {
+    public static void stopServiceOnMainThread(Context context) {
         context.startService(new Intent());
 
     }
 
-    private void subscribeToTopic() {
-//        FirebaseMessaging.getInstance().subscribeToTopic("all");
+    public static void subscribeToTopic() {
+        FirebaseMessaging.getInstance().subscribeToTopic("all");
+
+    }
+
+    public static void openImage(Activity activity, int SELECT_PICTURE) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        activity.startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+
+    public static void uploadImage(Uri uri, Context context) {
+        StorageReference mStorageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference riversRef = mStorageRef.child("images/" + System.currentTimeMillis() + (int) (Math.random() * 10000) + "." + getFileExtension(uri, context));
+        uri.getPath();
+
+        riversRef.putFile(uri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Task<Uri> downloadUrl = taskSnapshot.getStorage().getDownloadUrl();
+//                        URL:
+                        Uri uri1 = downloadUrl.getResult();
+                        System.out.println();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+    }
+
+    // Get Extension
+    public static String getFileExtension(Uri uri, Context context) {
+        ContentResolver contentResolver = context.getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+
+        // Return file Extension
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    public static Uri getUriFromData(Intent data) {
+        return data.getData();
+    }
+
+    public static void drive(Context context, String GKEY, GoogleMap mMap, String color, LatLng from, LatLng to, float zoom) {
+        DrawRoute drawRoute = new DrawRoute(new DrawRoute.onDrawRoute() {
+            @Override
+            public void afterDraw(String result) {
+            }
+        }, context);
+
+        drawRoute.setGmapAndKey(GKEY, mMap)
+                .setColorHash(color)
+                .setFromLatLong(from.latitude, from.longitude).setToLatLong(to.latitude, to.longitude)
+
+                .setZoomLevel(zoom).run();
+
+    }
+
+    public static void addMyLoc(Context context, Activity activity, final GoogleMap mMap, final LatLng myPos, final int bitmapResourseId) {
+
+        if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+            mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+
+                @SuppressLint("MissingPermission")
+                @Override
+                public void onMyLocationChange(Location location) {
+                    initOnLocChange(mMap, myPos, bitmapResourseId, location);
+                }
+            });
+
+
+        } else {
+            ActivityCompat.requestPermissions(activity, new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    1000);
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    public static void initOnLocChange(GoogleMap mMap, LatLng myPos, int bitmapResourseId, Location location) {
+
+        if (myPos == null) {
+            if (location != null) {
+                myPos = new LatLng(location.getLatitude(), location.getLongitude());
+            }
+
+            mMap.addMarker(new MarkerOptions().position(myPos).icon(BitmapDescriptorFactory.fromResource(bitmapResourseId)));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myPos, 12f));
+            mMap.setMyLocationEnabled(false);
+        }
+    }
+
+
+    public static void initBarChart(BarChart barChart, ArrayList<BarEntry> list, String label, String desc) {
+
+
+        BarDataSet barDataSet = new BarDataSet(list, label);
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(15f);
+
+        BarData barData = new BarData(barDataSet);
+
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.getDescription().setText(desc);
 
     }
 
 
+    public static void initPieChart(PieChart pieChart, ArrayList<PieEntry> list, String label, String desc) {
+
+
+        PieDataSet pieDataSet = new PieDataSet(list, label);
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(15f);
+
+        PieData pieData = new PieData(pieDataSet);
+
+        pieChart.setData(pieData);
+        pieChart.getDescription().setEnabled(true);
+        pieChart.setCenterText(label);
+        pieChart.setTransparentCircleRadius(0);
+        pieChart.setHoleRadius(0);
+        pieChart.getDescription().setText(desc);
+        pieChart.animate();
+    }
+
+
+    public static void initLineChart(LineChart lineChart, ArrayList<Entry> list, String label, String desc) {
+
+        LineDataSet lineDataSet = new LineDataSet(list, label);
+        lineDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        lineDataSet.setValueTextColor(Color.BLACK);
+        lineDataSet.setValueTextSize(15f);
+
+        LineData lineData = new LineData(lineDataSet);
+
+        lineChart.setData(lineData);
+        lineChart.getDescription().setEnabled(true);
+        lineChart.getDescription().setText(desc);
+        lineChart.animate();
+
+    }
 
 
 }
